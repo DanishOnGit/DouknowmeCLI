2486439508b3819a02793e104b05485f9da5932f
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-02-22 12:40:28 +0530

    refactor: variable declarations

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var chalk=require("chalk");
-var readlineSync=require("readline-sync");
+const chalk=require("chalk");
+const readlineSync=require("readline-sync");
 console.log(chalk.bold.yellow.underline("Let\'s play a Game!\n"));
-var userName=readlineSync.question(chalk.blue("Enter your Name: "));
+const userName=readlineSync.question(chalk.blue("Enter your Name: "));
 console.log("\nHi! "+chalk.rgb(51,51,255)(userName.toUpperCase())+".Welcome to the \"\DO YOU KNOW Danish quiz!\"\n");
 console.log(chalk.bgBlue("Answer the following questions.\nCorrect answer gives you 1 point\nNo negative marking.\n"))
-var score=0;
+let score=0;
 function play(question,answer){
-  var userAnswer=readlineSync.question(chalk.cyan(question));
+  let userAnswer=readlineSync.question(chalk.cyan(question));
   if(userAnswer.toLowerCase()==answer){
     console.log(chalk.green("Your are Right!\n"));
     score=score+1;
@@ -19,7 +19,7 @@ function play(question,answer){
      console.log("----------------------\n");
   }
 }
-var questions=[{question:"Q1)Where do I live? ",                               answer:"pune"},
+const questions=[{question:"Q1)Where do I live? ",                               answer:"pune"},
                 {question:"Q2)Which is my favourite colour? ",
                 answer:"black"},
                 {question:"Q3)Have I ever lived in USA? ",
